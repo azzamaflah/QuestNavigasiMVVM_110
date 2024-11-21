0a16f75fcc02766427b49ae1fb61d9515e4cabc0
azzamaflah <azzamhebat46@gmail.com>
2024-11-21 23:20:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mvvm/model/DataSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.mvvm.model
+
+data class DataSiswa(
+    val nama : String="",
+    val gender : String="",
+    val email : String="",
+    val nim : String="",
+    val alamat : String="",
+    val notelpon:String=""
+)
+
