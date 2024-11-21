98b8f489f9ff7669633f46800332d5b2ad125fdc
azzamaflah <azzamhebat46@gmail.com>
2024-11-21 23:31:21 +0700

    Menambahkan SiswaViewModel untuk mengelola data siswa dengan fungsi saveDataSiswa

--- app/src/main/java/com/example/mvvm/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/mvvm/viewmodel/SiswaViewModel.kt
@@ -1,4 +1,29 @@
 package com.example.mvvm.viewmodel
 
-class SiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.mvvm.model.DataSiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SiswaViewModel : ViewModel() {
+    //Request
+    private val _statusUI = MutableStateFlow(DataSiswa())
+
+    //Response
+    val statusUI: StateFlow<DataSiswa> = _statusUI.asStateFlow()
+
+    fun saveDataSiswa(ls: MutableList<String>) {
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nama = ls[0],
+                gender = ls[1],
+                email = ls[2],
+                nim = ls[3],
+                alamat = ls[4],
+                notelpon = ls[5]
+            )
+        }
+    }
 }
